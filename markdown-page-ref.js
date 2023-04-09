416a4390023b53a781182c6e8a4f8c584d080fb7
RuneImp <runeimp@gmail.com>
2023-04-09 11:14:37 -0700

    fixed YouTub non-watch pages markdown generation

--- markdown-page-ref.js
+++ markdown-page-ref.js
@@ -1,25 +1,25 @@
 /*
 M↓ Page Ref
 
-Version: 1.2.0
+Version: 1.2.1
 Description: YouTube and Rust Aware
-Updated: 2023-04-05
+Updated: 2023-04-09
 */
 javascript:(
 	() => {
-		let D=document, H=location.hostname, title=D.title;
+		let D=document, title=D.title;
 
 		switch(true) {
-		case H.indexOf('crates.io') !== -1:
+		case D.URL.indexOf('crates.io') !== -1:
 			title = D.title.replace('crates.io: Rust Package Registry', 'Crates.io');
 			break;
-		case H.indexOf('docs.rs') !== -1:
+		case D.URL.indexOf('docs.rs') !== -1:
 			title = D.title.replace('Rust', 'Docs.rs');
 			break;
-		case H.indexOf('lib.rs') !== -1:
+		case D.URL.indexOf('lib.rs') !== -1:
 			title = D.title.replace(' // ', ' - ').replace('—', '-');
 			break;
-		case  H.indexOf('youtube.com') !== -1:
+		case D.URL.indexOf('youtube.com/watch') !== -1:
 			let channel = D.querySelector('div#owner #text > a').innerText;
 			let duration = D.querySelector('#movie_player .ytp-time-duration').innerText;
 			let posted = (new Date(D.querySelector('#description #tooltip').innerText.split('•')[1].trim())).toISOString().substr(0, 10)
